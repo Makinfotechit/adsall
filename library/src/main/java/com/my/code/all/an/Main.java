363ab8b613a010c57d236b654f477bba3cdd6b62
makinfotech203 <makinfotech203@gmail.com>
2022-04-15 17:05:15 +0530

    new call

--- library/src/main/java/com/my/code/all/an/Main.java
+++
@@ -1,5 +0,0 @@
-package com.my.code.all.an;
-
-public class Main {
-    public static String vishal = "Vishal";
-}
--- library/src/main/java/com/my/code/all/an/Test.java
+++
@@ -1,7 +0,0 @@
-package com.my.code.all.an;
-
-public class Test {
-
-    public static int code = 1000;
-    public static int main = 1000;
-}
